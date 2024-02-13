547d900a7b1d8668736cbd74fa185bea05eb74eb
DmitriyKaplan28 <dsk007@mail.ru>
2024-02-13 21:55:28 +0300

    2622. Cache With Time Limit

---
+++ 2622. Cache With Time Limit.js
@@ -0,0 +1,41 @@
+var TimeLimitedCache = function () {
+    this.cache = {};
+};
+
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+    const currentTime = Date.now();
+
+    if (this.cache.hasOwnProperty(key) && this.cache[key].expiration > currentTime) {
+        this.cache[key].value = value;
+        this.cache[key].expiration = currentTime + duration;
+        return true;
+    } else {
+        this.cache[key] = {
+            value: value,
+            expiration: currentTime + duration
+        };
+        return false;
+    }
+};
+
+TimeLimitedCache.prototype.get = function (key) {
+    const currentTime = Date.now();
+
+    if (this.cache.hasOwnProperty(key) && this.cache[key].expiration > currentTime) {
+        return this.cache[key].value;
+    } else {
+        return -1;
+    }
+};
+
+TimeLimitedCache.prototype.count = function () {
+    const currentTime = Date.now();
+    let count = 0;
+    for (const key in this.cache) {
+        if (this.cache[key].expiration > currentTime) {
+            count++;
+        }
+    }
+
+    return count;
+};
