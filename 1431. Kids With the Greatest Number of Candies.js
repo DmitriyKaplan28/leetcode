9045216144a238d6edd132bea13286bd30c72470
DmitriyKaplan28 <dsk007@mail.ru>
2024-05-13 09:41:12 +0300

    1431. Kids With the Greatest Number of Candies

---
+++ 1431. Kids With the Greatest Number of Candies.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+  let result = [];
+  const max = Math.max(...candies);
+  candies.forEach((candy) => {
+    result.push(candy + extraCandies >= max);
+  });
+
+  return result;
+};
