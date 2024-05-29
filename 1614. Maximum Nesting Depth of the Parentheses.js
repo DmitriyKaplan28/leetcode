820c78856672af5238985c35f6e2990cbb802f47
DmitriyKaplan28 <dsk007@mail.ru>
2024-05-30 00:40:18 +0300

    fix: braces

--- 1614. Maximum Nesting Depth of the Parentheses.js
+++ 1614. Maximum Nesting Depth of the Parentheses.js
@@ -2,18 +2,19 @@
  * @param {string} s
  * @return {number}
  */
-var maxDepth = function(s) {
-    let depth = 0;
-    let max = 0;
-    for (let i = 0; i < s.length; i++) {
-     if (s[i] === "(") {
-       depth++;
-     }
-     if (s[i] === ")") {
-       depth--;
-     }
-     if (max < depth) {
-       max = depth;
-     }
+var maxDepth = function (s) {
+  let depth = 0;
+  let max = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      depth++;
     }
-    return max;
+    if (s[i] === ")") {
+      depth--;
+    }
+    if (max < depth) {
+      max = depth;
+    }
+  }
+  return max;
+};
