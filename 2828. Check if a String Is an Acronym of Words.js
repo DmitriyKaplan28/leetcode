4a1bc453dd860d0dcc8cd4ac0a506278230e917f
DmitriyKaplan28 <dsk007@mail.ru>
2024-06-21 10:24:34 +0300

    2828. Check if a String Is an Acronym of Words

---
+++ 2828. Check if a String Is an Acronym of Words.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {boolean}
+ */
+var isAcronym = function (words, s) {
+  let acronym = "";
+  words.forEach((word) => (acronym += word[0]));
+  return s === acronym;
+};
