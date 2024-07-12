903cd8e57cfb6ed1dea186edc095c23ec50ad258
DmitriyKaplan28 <dsk007@mail.ru>
2024-07-12 10:08:18 +0300

    update

--- 2124. Check if All A's Appears Before All B's.js
+++ 2124. Check if All A's Appears Before All B's.js
@@ -2,23 +2,8 @@
  * @param {string} s
  * @return {boolean}
  */
-var checkString = function(s) {
-    const indicesA = findAllOccurrences(s, "a")
-    const indicesB = findAllOccurrences(s, "b")
-    if (!indicesA.length || !indicesB.length) return true
-    return indicesA[indicesA.length-1] < indicesB[0]
+var checkString = function (s) {
+  let lastA = s.lastIndexOf("a");
+  let firstB = s.indexOf("b");
+  return firstB === -1 ? true : lastA < firstB;
 };
-
-const findAllOccurrences = (str, substr) => {
-    str = str.toLowerCase();
-    
-    let result = [];
-  
-    let idx = str.indexOf(substr)
-    
-    while (idx !== -1) {
-      result.push(idx);
-      idx = str.indexOf(substr, idx+1);
-    }
-    return result;
-  }
