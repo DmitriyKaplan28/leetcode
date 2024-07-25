66e5f0fcb2893c028dff8e5f67f9b6e521b06676
DmitriyKaplan28 <dsk007@mail.ru>
2024-07-25 09:24:55 +0300

    2351. First Letter to Appear Twice

---
+++ 2351. First Letter to Appear Twice.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var repeatedCharacter = function(s) {
+    const set = new Set()
+    for (const letter of s) {
+        if (set.has(letter)) return letter
+        else set.add(letter)
+    }
+};
