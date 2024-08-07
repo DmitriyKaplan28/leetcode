9cd02d4d89eb395094a0157f6165fd907cbc2d99
DmitriyKaplan28 <dsk007@mail.ru>
2024-08-08 00:58:54 +0300

    2733. Neither Minimum nor Maximum

---
+++ 2733. Neither Minimum nor Maximum.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNonMinOrMax = function(nums) {
+    const minAndMax = [Math.min(...nums), Math.max(...nums)]
+    const noMinOrMax = nums.find(num => !minAndMax.includes(num))
+    return noMinOrMax ? noMinOrMax : -1
+};
+console.log(findNonMinOrMax([1,2,3,4]))
