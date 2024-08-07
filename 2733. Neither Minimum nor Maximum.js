e13d26086fce28a7eca3ef3aa2a66981f11382f4
DmitriyKaplan28 <dsk007@mail.ru>
2024-08-08 00:59:22 +0300

    style fix

--- 2733. Neither Minimum nor Maximum.js
+++ 2733. Neither Minimum nor Maximum.js
@@ -2,9 +2,9 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findNonMinOrMax = function(nums) {
-    const minAndMax = [Math.min(...nums), Math.max(...nums)]
-    const noMinOrMax = nums.find(num => !minAndMax.includes(num))
-    return noMinOrMax ? noMinOrMax : -1
+var findNonMinOrMax = function (nums) {
+  const minAndMax = [Math.min(...nums), Math.max(...nums)];
+  const noMinOrMax = nums.find((num) => !minAndMax.includes(num));
+  return noMinOrMax ? noMinOrMax : -1;
 };
-console.log(findNonMinOrMax([1,2,3,4]))
+console.log(findNonMinOrMax([1, 2, 3, 4]));
