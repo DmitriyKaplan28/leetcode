9ebf001adc21800c282db8cc01a532af2d1a6ccf
DmitriyKaplan28 <dsk007@mail.ru>
2024-08-21 09:25:32 +0300

    2363. Merge Similar Items

---
+++ 2363. Merge Similar Items.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} items1
+ * @param {number[][]} items2
+ * @return {number[][]}
+ */
+var mergeSimilarItems = function (items1, items2) {
+    const map = new Map();
+    for (const [value, weight, ] of items1) {
+        map.set(value, weight);
+    }
+    for (const [value, weight, ] of items2) {
+        map.set(value, (map.has(value) ? map.get(value) : 0) + weight);
+    }
+    return [...map.entries(), ].sort((a, b) => a[0] - b[0]);
+};
+
+
+console.log(
+  mergeSimilarItems(
+    [
+      [1, 1],
+      [4, 5],
+      [3, 8],
+    ],
+    [
+      [3, 1],
+      [1, 5],
+    ]
+  )
+);
