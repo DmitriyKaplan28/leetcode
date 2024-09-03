533353432bbcd78c9feba45b07eba650c37c4774
DmitriyKaplan28 <dsk007@mail.ru>
2024-09-03 21:12:06 +0300

    500. Keyboard Row

---
+++ 500. Keyboard Row.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function (words) {
+  const rows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+  return words.filter((word) =>
+    rows.some((row) =>
+      word
+        .toLowerCase()
+        .split("")
+        .every((s) => row.includes(s))
+    )
+  );
+};
