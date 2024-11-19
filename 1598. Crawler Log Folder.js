6c1ecdb6e5bf37064180b6db5b436c7203f96406
DmitriyKaplan28 <dsk007@mail.ru>
2024-11-19 21:13:01 +0300

    1598. Crawler Log Folder

---
+++ 1598. Crawler Log Folder.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} logs
+ * @return {number}
+ */
+var minOperations = function (logs) {
+  let depth = 0;
+
+  for (let log of logs) {
+    if (log === "../") {
+      if (depth > 0) {
+        depth--;
+      }
+    } else if (log !== "./") {
+      depth++;
+    }
+  }
+
+  return depth;
+};
+
+console.log(minOperations(["d1/", "d2/", "../", "d21/", "./"]));
