3d5d79c80fffbed2a0550fa9bc068e72c6a710c0
DmitriyKaplan28 <dsk007@mail.ru>
2024-12-09 09:20:16 +0300

    905. Sort Array By Parity

---
+++ 905. Sort Array By Parity.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function (nums) {
+  let even = [];
+  let odd = [];
+
+  for (let num of nums) {
+    if (num % 2 === 0) {
+      even.push(num);
+    } else {
+      odd.push(num);
+    }
+  }
+
+  return [...even, ...odd];
+};
