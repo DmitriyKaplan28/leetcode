890a4e44b96cea7191accee13b99a80f0d2e48fa
DmitriyKaplan28 <dsk007@mail.ru>
2025-04-07 10:44:57 +0300

    5. Longest Palindromic Substring

--- 5. Longest Palindromic Substring.js
+++ 5. Longest Palindromic Substring.js
@@ -2,28 +2,29 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function (s) {
-  let startIndex = 0;
-  let maxLength = 1;
-
-  function expandAroundMiddle(left, right) {
-    while (left >= 0 && right < s.length && s[left] === s[right]) {
-      const currentLength = right - left + 1;
-
-      // Used in the slice method for return
-      if (currentLength > maxLength) {
-        maxLength = currentLength;
-        startIndex = left;
-      }
-      left--;
-      right++;
+var longestPalindrome = function(s) {
+    if (!s || s.length < 2) return s;
+    
+    let start = 0;
+    let maxLength = 1;
+    
+    function expandAroundCenter(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            const currentLength = right - left + 1;
+            if (currentLength > maxLength) {
+                start = left;
+                maxLength = currentLength;
+            }
+            left--;
+            right++;
+        }
     }
-  }
-
-  for (let i = 0; i < s.length; i++) {
-    expandAroundMiddle(i - 1, i + 1);
-    expandAroundMiddle(i, i + 1);
-  }
-
-  return s.slice(startIndex, startIndex + maxLength);
+    
+    for (let i = 0; i < s.length; i++) {
+        expandAroundCenter(i, i);
+    
+        expandAroundCenter(i, i + 1);
+    }
+    
+    return s.substring(start, start + maxLength);
 };
