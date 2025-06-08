ea116c5fb467ea6c55283d6191ebb5e4bbe2c3ca
DmitriyKaplan28 <dsk007@mail.ru>
2025-06-08 14:08:26 +0300

    37. Sudoku Solver

---
+++ 37. Sudoku Solver.js
@@ -0,0 +1,62 @@
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solveSudoku = function (board) {
+  const isValid = (row, col, num) => {
+    for (let x = 0; x < 9; x++) {
+      if (board[row][x] === num) return false;
+    }
+
+    for (let x = 0; x < 9; x++) {
+      if (board[x][col] === num) return false;
+    }
+
+    const startRow = Math.floor(row / 3) * 3;
+    const startCol = Math.floor(col / 3) * 3;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[startRow + i][startCol + j] === num) return false;
+      }
+    }
+
+    return true;
+  };
+
+  const solve = () => {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (board[row][col] === ".") {
+          for (let num = 1; num <= 9; num++) {
+            const numStr = num.toString();
+            if (isValid(row, col, numStr)) {
+              board[row][col] = numStr;
+
+              if (solve()) return true;
+
+              board[row][col] = ".";
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+
+  solve();
+};
+
+console.log(
+  solveSudoku([
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+  ])
+);
