fb34aa79087f9ca6b7065f01deeaff326cb33c56
DmitriyKaplan28 <dsk007@mail.ru>
2025-06-17 21:01:09 +0300

    73. Set Matrix Zeroes

---
+++ 73. Set Matrix Zeroes.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+
+  const zeroRows = new Array(m).fill(false);
+  const zeroCols = new Array(n).fill(false);
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === 0) {
+        zeroRows[i] = true;
+        zeroCols[j] = true;
+      }
+    }
+  }
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (zeroRows[i] || zeroCols[j]) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+};
