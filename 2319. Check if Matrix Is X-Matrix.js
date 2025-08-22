3c4ce7b92e4d71b71cb15320306934b5ee4015c4
DmitriyKaplan28 <dsk007@mail.ru>
2025-08-22 09:10:02 +0300

    2319. Check if Matrix Is X-Matrix

---
+++ 2319. Check if Matrix Is X-Matrix.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} grid
+ * @return {boolean}
+ */
+var checkXMatrix = function (grid) {
+  const n = grid.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      const isOnDiagonal = i === j || i + j === n - 1;
+
+      if (isOnDiagonal) {
+        if (grid[i][j] === 0) {
+          return false;
+        }
+      } else {
+        if (grid[i][j] !== 0) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return true;
+};
