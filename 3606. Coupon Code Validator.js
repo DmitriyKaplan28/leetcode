cf09972bbce452e8efafe87d66761aa6da935a26
DmitriyKaplan28 <dsk007@mail.ru>
2025-09-14 17:25:57 +0300

    3606. Coupon Code Validator

---
+++ 3606. Coupon Code Validator.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} code
+ * @param {string[]} businessLine
+ * @param {boolean[]} isActive
+ * @return {string[]}
+ */
+var validateCoupons = function (code, businessLine, isActive) {
+  const B = {};
+  (B.electronics = []),
+    (B.grocery = []),
+    (B.pharmacy = []),
+    (B.restaurant = []);
+  for (let i = 0; i < code.length; i++) {
+    const s = code[i],
+      t = businessLine[i];
+    if (s && !~s.search(/\W/) && B[t] && isActive[i]) B[t].push(s);
+  }
+  const res = [];
+  for (const key in B) {
+    res.push(...B[key].sort());
+  }
+  return res;
+};
