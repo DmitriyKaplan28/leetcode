2bdff9ca7e452153e48058d674c73c098dab6dcd
DmitriyKaplan28 <101748283+DmitriyKaplan28@users.noreply.github.com>
2025-09-21 20:11:41 +0300

    2022. Convert 1D Array Into 2D Array

---
+++ 2022. Convert 1D Array Into 2D Array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} original
+ * @param {number} m
+ * @param {number} n
+ * @return {number[][]}
+ */
+var construct2DArray = function(original, m, n) {
+   if (original.length !== m * n) return [];
+
+  const result = new Array(m);
+  for (let i = 0; i < m; i++) {
+    result[i] = original.slice(i * n, (i + 1) * n);
+  }
+  return result; 
+};
