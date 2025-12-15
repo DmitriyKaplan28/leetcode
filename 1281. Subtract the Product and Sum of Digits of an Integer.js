dfad691435f6b0b024da442bdefb477f513915cc
DmitriyKaplan28 <dsk007@mail.ru>
2025-12-15 08:59:50 +0300

    1281. Subtract the Product and Sum of Digits of an Integer

---
+++ 1281. Subtract the Product and Sum of Digits of an Integer.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    let product = 1;
+    let sum = 0;
+    while (n > 0) {
+        const digit = n % 10;
+        product *= digit;
+        sum += digit;
+        n = Math.floor(n / 10);
+    }
+    return product - sum;
+};
+console.log(subtractProductAndSum(234));
+console.log(subtractProductAndSum(4421));
