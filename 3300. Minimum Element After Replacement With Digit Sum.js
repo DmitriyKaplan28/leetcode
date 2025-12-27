32b56c7f83cbe1234e777a78e19aab7a15576da6
DmitriyKaplan28 <dsk007@mail.ru>
2025-12-27 14:27:21 +0300

    3300. Minimum Element After Replacement With Digit Sum

---
+++ 3300. Minimum Element After Replacement With Digit Sum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minElement = function (nums) {
+  const digitSum = (num) => {
+    let sum = 0;
+    while (num > 0) {
+      sum += num % 10;
+      num = Math.floor(num / 10);
+    }
+    return sum;
+  };
+
+  let min = Infinity;
+
+  for (let num of nums) {
+    const sum = digitSum(num);
+    min = Math.min(min, sum);
+  }
+
+  return min;
+};
