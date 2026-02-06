76480063c8ce9eaf6d260bda7748b3690a3ac284
DmitriyKaplan28 <dsk007@mail.ru>
2026-02-06 09:34:12 +0300

    3194. Minimum Average of Smallest and Largest Elements

---
+++ 3194. Minimum Average of Smallest and Largest Elements.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumAverage = function (nums) {
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+    let minAvg = Infinity;
+    for (let i = 0; i < n / 2; i++) {
+        const avg = (nums[i] + nums[n - 1 - i]) / 2;
+        minAvg = Math.min(minAvg, avg);
+    }
+    return minAvg;
+};
