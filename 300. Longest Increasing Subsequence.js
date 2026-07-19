fbb58dace1d75d0800b64186ccf263c3f4747944
DmitriyKaplan28 <101748283+DmitriyKaplan28@users.noreply.github.com>
2026-07-19 15:59:51 +0300

    300. Longest Increasing Subsequence

---
+++ 300. Longest Increasing Subsequence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    const tails = [];
+    for (const num of nums) {
+        let left = 0, right = tails.length;
+        while (left < right) {
+            const mid = (left + right) >> 1;
+            if (tails[mid] < num) left = mid + 1;
+            else right = mid;
+        }
+        tails[left] = num;
+    }
+    return tails.length;
+};
