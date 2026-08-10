a8ec9d72a52d21ba5d3ae8571c5d32af2b903302
DmitriyKaplan28 <dsk007@mail.ru>
2026-08-10 10:51:30 +0300

    406. Queue Reconstruction by Height

---
+++ 406. Queue Reconstruction by Height.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} people
+ * @return {number[][]}
+ */
+var reconstructQueue = function (people) {
+  people.sort((a, b) => {
+    if (a[0] !== b[0]) return b[0] - a[0];
+    return a[1] - b[1];
+  });
+
+  const result = [];
+
+  for (const person of people) {
+    result.splice(person[1], 0, person);
+  }
+
+  return result;
+};
